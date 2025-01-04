45e7ebdd8d48a7a2d8595f042199a9c75d97ce9e
hashzzzzzz <erollhashani5@gmail.com>
2025-01-04 23:05:48 +0100

    Updated CORS to allow requests from GitHub Pages frontend

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ db.once('open', () => {
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL ||  'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
